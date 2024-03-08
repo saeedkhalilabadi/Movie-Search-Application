a7605917c494ee1d6251100f87a6e83b36e3cf31
saeedkhalilabadi <saeedkhalilabadi@gmail.com>
2024-03-09 01:11:33 +0330

    create pagination for search result

---
+++ src/components/pagination/index.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import Paginate from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+import { useState } from "react";
+import useQueryParams from "../../shared/hooks/useQueryParams";
+
+type PaginationProps = {
+  totalResults: number;
+  pageCount: string;
+};
+export default function Pagination(props: PaginationProps) {
+  const [page, setPage] = useState(1);
+  const { pageCount, totalResults } = props;
+  const count = Math.round(Number(totalResults) / Number(pageCount));
+  const { setPage: setPageQuery } = useQueryParams();
+
+  React.useEffect(() => {
+    setPageQuery(String(page));
+  }, [page]);
+
+  return (
+    count > 1 && (
+      <Stack spacing={2} className=" mx-auto justify-center">
+        <Paginate
+          count={count}
+          onChange={(e, value) => setPage(value)}
+          variant="outlined"
+          shape="rounded"
+        />
+      </Stack>
+    )
+  );
+}
