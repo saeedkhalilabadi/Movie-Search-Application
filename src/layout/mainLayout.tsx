000484b39b546891543880218c4e5b4ac3d8e54d
saeedkhalilabadi <saeedkhalilabadi@gmail.com>
2024-03-09 17:21:00 +0330

    change style: main layout width

--- src/layout/mainLayout.tsx
+++ src/layout/mainLayout.tsx
@@ -6,7 +6,10 @@ export default function MainLayout() {
   return (
     <div className="relative bg-yellow-600 w-full min-h-[100vh] pb-14">
       <Header />
-      <Outlet />
+      <section className="w-full max-w-[1580px] mx-auto">
+        <Outlet />
+      </section>
+
       <Footer />
     </div>
   );
